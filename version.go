6304c3c824058d2b88ef3f651c20c7092e118ca4
Feng Yifei <fengyfei@gmail.com>
2019-05-10 21:43:26 +0800

    routes support

---
+++ version.go
@@ -0,0 +1,11 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// Version get the version of api.
+func Version(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "API Version 1.0-alpha")
+}
