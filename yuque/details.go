5dbe76cbcbab8d7845efc2cd3ec1cf1349264cbc
yang <1351599172@qq.com>
2019-05-21 16:13:36 +0800

    creat dir yuque

---
+++ yuque/details.go
@@ -0,0 +1,143 @@
+package handler
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+// Details -
+var Details struct {
+	Abilities Abilities `json:"abilities"`
+	Data      Data      `json:"data"`
+}
+
+// BookDetail -
+func BookDetail(w http.ResponseWriter, r *http.Request) {
+	repoid := r.FormValue("RepoID")
+	id := r.FormValue("ID")
+	token := r.FormValue("Token")
+
+	host := "https://www.yuque.com"
+	url := host + "/api/v2/repos/" + repoid + "/docs/" + id + "?raw=0"
+
+	client := &http.Client{}
+
+	request, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	request.Header.Add("X-Auth-Token", token)
+
+	response, err := client.Do(request)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	err = json.Unmarshal(body, &Details)
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Fprintf(w, "%v", Details)
+}
+
+// Abilities -
+type Abilities struct {
+	Update  bool `json:"update"`
+	Destroy bool `json:"destroy"`
+}
+
+// Data -
+type Data struct {
+	ID     int64  `json:"id"`
+	Slug   string `json:"slug"`
+	Title  string `json:"title"`
+	BookID int64  `json:"book_id"`
+	Book   struct {
+		ID               int64  `json:"id"`
+		Type             string `json:"type"`
+		Slug             string `json:"slug"`
+		Name             string `json:"name"`
+		UserID           int64  `json:"user_id"`
+		Description      string `json:"description"`
+		CreatorID        int64  `json:"creator_id"`
+		Public           int    `json:"public"`
+		ItemsCount       int    `json:"items_count"`
+		LikesCount       int    `json:"likes_count"`
+		WatchesCount     int    `json:"watches_count"`
+		ContentUpdatedAt string `json:"content_updated_at"`
+		UpdatedAt        string `json:"updated_at"`
+		CreatedAt        string `json:"created_at"`
+		Namespace        string `json:"namespace"`
+		User             struct {
+			ID               int64  `json:"id"`
+			Type             string `json:"type"`
+			Login            string `json:"login"`
+			Name             string `json:"name"`
+			Description      string `json:"description"`
+			AvatarURL        string `json:"avatar_url"`
+			LargeAvatarURL   string `json:"large_avatar_url"`
+			MediumAvatarURL  string `json:"medium_avatar_url"`
+			SmallAvatarURL   string `json:"small_avatar_url"`
+			BooksCount       int    `json:"books_count"`
+			PublicBooksCount int    `json:"public_books_count"`
+			FollowersCount   int    `json:"followers_count"`
+			FollowingCount   int    `json:"following_count"`
+			CreatedAt        string `json:"created_at"`
+			UpdatedAt        string `json:"updated_at"`
+			Serializer       string `json:"_serializer"`
+		} `json:"user"`
+		Serializer string `json:"_serializer"`
+	} `json:"book"`
+	UserID  int64 `json:"user_id"`
+	Creator struct {
+		ID               int64  `json:"id"`
+		Type             string `json:"type"`
+		Login            string `json:"login"`
+		Name             string `json:"name"`
+		Description      string `json:"description"`
+		AvatarURL        string `json:"avatar_url"`
+		LargeAvatarURL   string `json:"large_avatar_url"`
+		MediumAvatarURL  string `json:"medium_avatar_url"`
+		SmallAvatarURL   string `json:"small_avatar_url"`
+		BooksCount       int    `json:"books_count"`
+		PublicBooksCount int    `json:"public_books_count"`
+		FollowersCount   int    `json:"followers_count"`
+		FollowingCount   int    `json:"following_count"`
+		CreatedAt        string `json:"created_at"`
+		UpdatedAt        string `json:"updated_at"`
+		Serializer       string `json:"_serializer"`
+	} `json:"creator"`
+	Format            string `json:"format"`
+	Body              string `json:"body"`
+	BodyDraft         string `json:"body_draft"`
+	BodyHTML          string `json:"body_html"`
+	BodyLake          string `json:"body_lake"`
+	Public            int    `json:"public"`
+	Status            int    `json:"status"`
+	LikesCount        int    `json:"likes_count"`
+	CommentsCount     int    `json:"comments_count"`
+	ContentUpdatedAt  string `json:"content_updated_at"`
+	DeletedAt         string `json:"deleted_at"`
+	CreatedAt         string `json:"created_at"`
+	UpdatedAt         string `json:"updated_at"`
+	PublishedAt       string `json:"published_at"`
+	FirstPublishedAt  string `json:"first_published_at"`
+	WordCount         int    `json:"word_count"`
+	Cover             string `json:"cover"`
+	Description       string `json:"description"`
+	CustomDescription string `json:"custom_description"`
+	Serializer        string `json:"_serializer"`
+}
---
+++ yuque/list.go
@@ -0,0 +1,105 @@
+package handler
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+// List -
+var List struct {
+	Data []Books `json:"data"`
+}
+
+// BookList -
+func BookList(w http.ResponseWriter, r *http.Request) {
+	var book = make(map[string]string)
+
+	repoid := r.FormValue("RepoID")
+	token := r.FormValue("Token")
+
+	host := "https://www.yuque.com"
+	url := host + "/api/v2/repos/" + repoid + "/docs/"
+
+	client := &http.Client{}
+
+	request, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	request.Header.Add("X-Auth-Token", token)
+
+	response, err := client.Do(request)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	err = json.Unmarshal(body, &List)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	for _, v := range List.Data {
+		bookID := strconv.FormatInt(v.ID, 10)
+		book[v.Title] = bookID
+	}
+
+	fmt.Fprintf(w, "%v", book)
+}
+
+// Books -
+type Books struct {
+	ID               int64  `json:"id"`
+	Slug             string `json:"slug"`
+	Title            string `json:"title"`
+	Description      string `json:"description"`
+	UserID           int64  `json:"user_id"`
+	BookID           int64  `json:"book_id"`
+	Format           string `json:"format"`
+	Public           int    `json:"public"`
+	Status           int    `json:"status"`
+	LikesCount       int    `json:"likes_count"`
+	CommentsCount    int    `json:"comments_count"`
+	ContentUpdated   string `json:"content_updated_at"`
+	CreatedAt        string `json:"created_at"`
+	UpdatedAt        string `json:"updated_at"`
+	PublishedAt      string `json:"published_at"`
+	FirstPubilshedAt string `json:"first_published_at"`
+	DraftVersion     int    `json:"draft_version"`
+	LastEditorID     int32  `json:"last_editor_id"`
+	WordCount        int    `json:"word_count"`
+	Cover            string `json:"cover"`
+	Custom           string `json:"custom_description"`
+	LastEditor       struct {
+		ID              int64  `json:"id"`
+		Type            string `json:"type"`
+		Login           string `json:"login"`
+		Name            string `json:"name"`
+		Description     string `json:"description"`
+		AvatarURL       string `json:"avatar_url"`
+		LargeAvatarURL  string `json:"large_avatar_url"`
+		MediumAvatarURL string `json:"medium_avatar_url"`
+		SmallAvatarURL  string `json:"small_avatar_url"`
+		FollowersCount  int    `json:"followers_count"`
+		FollowingCount  int    `json:"following_count"`
+		CreatedAt       string `json:"created_at"`
+		UpdatedAt       string `json:"updated_at"`
+		Serializer      string `json:"_serializer"`
+	} `json:"last_editor"`
+	Book       string `json:"book"`
+	Serializer string `json:"_serializer"`
+}
