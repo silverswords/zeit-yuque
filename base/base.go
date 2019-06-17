0ba5ee800dce8bc5651934620140b27be57177a0
yang <1351599172@qq.com>
2019-06-17 17:10:07 +0800

    Create base.go

---
+++ base/base.go
@@ -0,0 +1,10 @@
+package yuque
+
+var (
+	// DetailURL -
+	DetailURL = "https://www.yuque.com/api/v2/repos/%s/docs/%s?raw=0"
+	// ListURL -
+	ListURL = "https://www.yuque.com/api/v2/repos/%s/docs/"
+	// RepoURL -
+	RepoURL = "https://www.yuque.com/api/v2/groups/%s/repos/"
+)
